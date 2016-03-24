b57d1b494b6acd41e36fdf6fc3d351f211501bf5
jkbits1 <jkbits1@users.noreply.github.com>
2016-03-24 22:39:22 +0000

    get multiple strings at fixed positions

--- server.js
+++ server.js
@@ -6,8 +6,41 @@
 
 const fs = require('fs');
 
-fs.readFile('test.bin', (err, data) => {
+let positions = [0x80, 0x180, 0x29A, 0x619];
+let strings = new Array(positions.length);
+
+fs.readFile('test2.bin', (err, data) => {
   let info = data.toString();
 
-  console.error(info);
+  let zero = data.indexOf(0);
+
+  //console.error(strings.length);
+
+  let terms = positions.map((pos, idx) => {
+    let term = data.indexOf(0, pos);
+    let fileString = data.slice(pos, term);
+
+    strings[idx] = new Buffer(term - pos + 1);
+
+    // buffers are init'ed, so just in case copy fails
+    strings[idx].fill(0);
+
+    fileString.copy(strings[idx]);
+
+    //console.error(strings[idx].toString());
+
+    return term;
+  });
+
+  terms.forEach(term => {
+
+    //console.error("term -", term);
+  });
+
+  strings.forEach(str => {
+    console.error(str.toString());
+  });
+
+  //console.error("info:", info);
+  //console.error(zero);
 });
